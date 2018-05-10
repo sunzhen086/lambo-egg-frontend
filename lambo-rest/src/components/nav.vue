a0b1ae64898d54a7bf13764b78e0949c0d53169c
cuiyuhua <cuiyh01@163.com>
2018-05-10 09:49:14 +0800

    初始版本

---
+++ lambo-rest/src/components/nav.vue
@@ -0,0 +1,51 @@
+<template>
+  <Layout>
+    <Header>
+      <Menu mode="horizontal" theme="dark" active-name="1">
+        <MenuItem name="服务定义">
+          <Icon type="ios-paper"></Icon>
+          服务定义
+        </MenuItem>
+        <MenuItem name="2">
+          <Icon type="ios-people"></Icon>
+          用户管理
+        </MenuItem>
+        <Submenu name="3">
+          <template slot="title">
+            <Icon type="stats-bars"></Icon>
+            统计分析
+          </template>
+          <MenuGroup title="使用">
+            <MenuItem name="3-1">新增和启动</MenuItem>
+            <MenuItem name="3-2">活跃分析</MenuItem>
+            <MenuItem name="3-3">时段分析</MenuItem>
+          </MenuGroup>
+          <MenuGroup title="留存">
+            <MenuItem name="3-4">用户留存</MenuItem>
+            <MenuItem name="3-5">流失用户</MenuItem>
+          </MenuGroup>
+        </Submenu>
+        <MenuItem name="4">
+          <Icon type="settings"></Icon>
+          综合设置
+        </MenuItem>
+      </Menu>
+    </Header>
+    <Content class="content">
+      <router-view></router-view>
+    </Content>
+  </Layout>
+
+</template>
+
+<script>
+    export default {
+        name: "nav"
+    }
+</script>
+
+<style lang="less" scoped>
+  .content{
+    padding-bottom:20px;
+  }
+</style>
---
+++ lambo-rest/src/components/restsetting/restsetting.vue
@@ -0,0 +1,117 @@
+<template>
+  <Layout>
+    <Sider hide-trigger :style="{background: '#fff'}" width="300">
+      <Tree :data="data1"></Tree>
+    </Sider>
+    <Content>
+      <lambo-edit-table ref="table1"  v-model="datas" :columns="columns"></lambo-edit-table>
+    </Content>
+  </Layout>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        data1: [
+          {
+            title: 'parent 1',
+            expand: true,
+            children: [
+              {
+                title: 'parent 1-1',
+                expand: true,
+                children: [
+                  {
+                    title: 'leaf 1-1-1'
+                  },
+                  {
+                    title: 'leaf 1-1-2'
+                  }
+                ]
+              },
+              {
+                title: 'parent 1-2',
+                expand: true,
+                children: [
+                  {
+                    title: 'leaf 1-2-1'
+                  },
+                  {
+                    title: 'leaf 1-2-1'
+                  }
+                ]
+              }
+            ]
+          }
+        ],
+        columns: [
+          {
+            title: '序号',
+            type: 'index',
+            width: 180,
+            align: 'center'
+          },
+          {
+            title: '姓名',
+            align: 'center',
+            key: 'name',
+            width: 190,
+            editor:{
+              type:"text",
+              //校验函数,参数分别为：新值、旧值、行数据、行号
+              validate:function(newVal,oldVal,row,index){
+                if(newVal.trim() == ""){
+                  return{
+                    valid:false,
+                    msg:"输入不能为空！"
+                  }
+                }
+                return{valid:true}
+              }
+            }
+          },
+          {
+            title: '性别',
+            align: 'center',
+            width: 150,
+            key: 'sex',
+            editor:{
+              type:"select",
+              enums:[{"value":"1","label":"男"},{"value":"0","label":"女"}]
+            }
+          },
+          {
+            title: '组织',
+            align: 'center',
+            key: 'organName'
+          }
+        ],
+        datas: [
+          {
+            name: 'Aresn',
+            sex: '1',
+            organId: '4',
+            organName: '河北分部'
+          },
+          {
+            name: 'Lison',
+            sex: '1',
+            organId: '5',
+            organName: '河南分部'
+          },
+          {
+            name: 'lisa',
+            sex: '0',
+            organId: '5',
+            organName: '河南分部'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
