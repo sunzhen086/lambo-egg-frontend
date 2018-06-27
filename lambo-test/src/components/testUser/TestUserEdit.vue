c4cf2cb7e81daae6536e1577cdf6e17cc5e59300
wangjierj <931649248@qq.com>
2018-06-27 10:02:22 +0800

    王洁 修改

--- lambo-test/src/components/testUser/TestUserEdit.vue
+++ lambo-test/src/components/testUser/TestUserEdit.vue
@@ -10,19 +10,24 @@
                     <Icon type="android-arrow-back"></Icon> 返回</a>
             </div>
             <Form  ref="formData" :model="formData" :label-width="80" :rules="ruleValidate">
-                                                                                                <FormItem label="user_name" prop="userName">
-                            <Input v-model="formData.userName" placeholder="请输入user_name"></Input>
-                        </FormItem>
-                                                                                <FormItem label="sex" prop="userName">
-                            <Input v-model="formData.sex" placeholder="请输入sex"></Input>
-                        </FormItem>
-                                                                                <FormItem label="age" prop="userName">
-                            <Input v-model="formData.age" placeholder="请输入age"></Input>
-                        </FormItem>
-                                                                                <FormItem label="ctime" prop="userName">
-                            <Input v-model="formData.ctime" placeholder="请输入ctime"></Input>
-                        </FormItem>
-                                                </Form>
+                                                                                                                                <FormItem label="user_id" prop="userId">
+                                <div v-if="userId">{{formData.userId}}</div>
+                                
+                                <Input v-model="formData.userId" placeholder="请输入user_id" v-else></Input>
+                            </FormItem>
+                                                                                                                                                                            <FormItem label="user_name" prop="userName">
+                                <Input v-model="formData.userName" placeholder="请输入user_name"></Input>
+                            </FormItem>
+                                                                                                                                                                            <FormItem label="sex" prop="sex">
+                                <Input v-model="formData.sex" placeholder="请输入sex"></Input>
+                            </FormItem>
+                                                                                                                                                                            <FormItem label="age" prop="age">
+                                <Input v-model="formData.age" placeholder="请输入age"></Input>
+                            </FormItem>
+                                                                                                                                                                            <FormItem label="ctime" prop="ctime">
+                                <Input v-model="formData.ctime" placeholder="请输入ctime"></Input>
+                            </FormItem>
+                                                                        </Form>
         </Card>
     </div>
 </template>
@@ -34,7 +39,8 @@
         data () {
             return {
                 formData: {
-                                                                                                                    userName:"",
+                                                                        userId:"",
+                                                                                                userName:"",
                                                                                                 sex:"",
                                                                                                 age:"",
                                                                                                 ctime:"",
@@ -59,7 +65,8 @@
             formSubmit(){
                 var self = this;
                 var params = {
-                                                                                                                    userName:self.formData.userName,
+                                                                        userId:self.formData.userId,
+                                                                                                userName:self.formData.userName,
                                                                                                 sex:self.formData.sex,
                                                                                                 age:self.formData.age,
                                                                                                 ctime:self.formData.ctime,
@@ -82,7 +89,8 @@
                 if(self.userId) { //修改
                     util.ajax.get("/manage/testUser/get/" + self.userId).then(function(resp) {
                         var result = resp.data;
-                                                                                                                                        self.formData.userName = result.userName;
+                                                                                    self.formData.userId = result.userId;
+                                                                                                                self.formData.userName = result.userName;
                                                                                                                 self.formData.sex = result.sex;
                                                                                                                 self.formData.age = result.age;
                                                                                                                 self.formData.ctime = result.ctime;
